50c83765fa5c5d5d09df897a26a05db7af26a058
quni <quni.inne@gmail.com>
2024-07-22 20:26:16 +0200

    ✨ feat: enabling CORS for all routes && adding error handling

--- backend/index.ts
+++ backend/index.ts
@@ -1,23 +1,33 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import jsonServer from 'json-server';
-
+import cors from 'cors';
 import path from 'path';
+import chalk from 'chalk'
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve('build')));
 
-app.use(function(_, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+app.use(cors());
+
+app.use(express.json());
 
 app.use('/api', jsonServer.router('db.json'));
 
+app.use((_: Request, res: Response) => {
+  res.status(404).json({ error: 'Not found' });
+});
+
+app.use((err: Error, _: Request, res: Response, __: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
+});
 
 app.listen(port, () => {
-  
-    console.log(`server started on port ${port}`);
+  console.log(chalk.yellow('---------------------------'));
+  console.log(chalk.dim('Press CTRL + C to stop the server'));
+  console.log(`Server started on http://localhost:${port}`);
+  console.log(chalk.dim(`Navigate to the posts endpoint: http://localhost:${port}/api/posts`));
+  console.log(chalk.yellow('---------------------------'));
 });
